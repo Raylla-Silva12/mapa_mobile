d796adedaa7adf588168306c5eba10e36f202945
Raylla-Silva12 <raylla.l.silva@gmail.com>
2021-11-27 10:30:27 +0900

    Versão 0.2

--- www/components/mapa.js
+++ www/components/mapa.js
@@ -1,16 +1,34 @@
-window.onload = function (){
+window.onload = function () {
 
-  document.querySelector("#botao").addEventListener("click", function (){
-    
+  function mapa( lat, long) {
+
     L.mapquest.key = 'uuUlD43vKBqjsyE4G0fWtb5A4erx6yro';
 
-        var map = L.mapquest.map('map', {
-          center: [37.7749, -122.4194],
-          layers: L.mapquest.tileLayer('map'),
-          zoom: 12
-        });
+    var map = L.mapquest.map('map', {
+      center: [lat, long],
+      layers: L.mapquest.tileLayer('map'),
+      zoom: 12
+    });
 
-        map.addControl(L.mapquest.control());
+    map.addControl(L.mapquest.control());
+
+  }
+
+  document.querySelector("#botao").addEventListener("click", function () {
+
+    var onSuccess = function (position) {
+      mapa(position.coords.latitude, position.coords.longitude);
+
+    };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+      alert('code: ' + error.code + '\n' +
+            'message: ' + error.message + '\n');
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
   });
 
